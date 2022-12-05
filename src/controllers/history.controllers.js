6c85f3da6eccf5c2d08c42c5c15bf39684cb2f9e
sheilagj19 <111460663+SheilaJacquemin@users.noreply.github.com>
2022-12-05 10:50:41 -0300

    controladores para tareas diarias

--- src/controllers/history.controllers.js
+++ src/controllers/history.controllers.js
@@ -1,52 +1,76 @@
-const getFormatDate = require("../helpers/getFormatDate")
-const History = require("../models/history.model")
+const getFormatDate = require("../helpers/getFormatDate");
+const History = require("../models/history.model");
 
-const controller = {}
+const controller = {};
 
 controller.getHistory = async (req, res) => {
   try {
-    const history = await History.find({})
+    const history = await History.find({});
     return res.status(200).json({
-      message: 'Historial del tareas',
-      history
-    })
-  } catch (error) {
-
-  }
-}
+      message: "Historial del tareas",
+      history,
+    });
+  } catch (error) {}
+};
 
 controller.getDailyHistory = async (req, res) => {
-
   try {
-    const { full } = getFormatDate()
-    const { user } = req
-    const findHistory = await History.findOne({ date: full, user: user._id })
+    const { full } = getFormatDate();
+    const { user } = req;
+    const findHistory = await History.findOne({ date: full, user: user._id });
+    const days = await History.count({user: user._id})
     let history;
-    let created = false
+    let created = false;
 
-    console.log(user)
+    console.log(user);
 
     if (findHistory) {
-      history = findHistory
+      history = findHistory;
     } else {
       history = new History({
         user: user._id,
         date: full,
-        habits: user.habits
-      })
-      await history.save()
-      created = true
+        habits: user.habits,
+      });
+      await history.save();
+      created = true;
     }
 
     return res.status(200).json({
-      message: 'peticion exitosa',
+      message: "peticion exitosa",
       history,
-      isNew: created
-    })
+      days,
+      isNew: created,
+    });
   } catch (error) {
-    console.log(error)
-    return res.status(500).json({ message: 'error' })
+    console.log(error);
+    return res.status(500).json({ message: "error" });
   }
-}
+};
 
-module.exports = controller
+controller.updateDailyHistory = async (req, res) => {
+  try {
+    const { full } = getFormatDate();
+    const { user } = req;
+    const { habits } = req.body;
+    const history = await History.findOne({ date: full, user: user._id });
+    if (history) {
+      history.habits = habits;
+      await history.save();
+      return res.status(200).json({
+        message: "Los datos se guardaron exitosamente",
+        history,
+      });
+    }
+    return res.status(400).json({
+      message: "No existe un seguimiento diario de los datos",
+    });
+  } catch (error) {
+    console.log("Error al actualizar daily history =>", error);
+    return res.status(500).json({
+      message: "Error en el servidor",
+    });
+  }
+};
+
+module.exports = controller;
--- src/routes/history.routes.js
+++ src/routes/history.routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
-const { getHistory, getDailyHistory } = require('../controllers/history.controllers')
+const { getHistory, getDailyHistory, updateDailyHistory } = require('../controllers/history.controllers')
 const validarJWT = require('../middlewares/validarJWT')
 
 router.get("/history", getHistory)
 router.get("/history/daily", validarJWT, getDailyHistory)
+router.put("/history/daily", validarJWT, updateDailyHistory)
 
 module.exports = router
